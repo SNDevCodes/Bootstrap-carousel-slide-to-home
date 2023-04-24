89fbc6f65c291591af695de0355725b2e97acd53
Sumanth Naik <108412073+SNDevCodes@users.noreply.github.com>
2023-04-24 11:01:50 +0530

    Create inde.js

---
+++ inde.js
@@ -0,0 +1,18 @@
+     <script>
+        // JavaScript code
+const myCarousel = document.getElementById('carouselExampleIndicators');
+
+myCarousel.addEventListener('slide.bs.carousel', event => {
+  // Prevent the default carousel behavior
+//   event.preventDefault();
+
+  // Determine the current slide index
+  const currentSlideIndex = Array.from(myCarousel.querySelectorAll('.carousel-item')).indexOf(event.relatedTarget);
+
+
+  // Perform custom actions based on the current slide index
+  if (currentSlideIndex ===  2) {
+    window.location.href = history.back();
+  } 
+});
+      </script>
